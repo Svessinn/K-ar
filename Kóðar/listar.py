901fe82427663098448d9e67f12484b696dc8748
Sverrir Hákon <24295046+Svessinn@users.noreply.github.com>
2019-02-20 22:50:00 +0000

    Add files via upload

---
+++ Kóðar/listar.py
@@ -0,0 +1,31 @@
+## Listar
+## Býr til listann lst
+lst = ['x', 'y', 'z', 1, 2, 3, 1, 2, 3]
+print('listinn í byrjun', lst)
+## Bætir við listann lst
+lst.append('x')
+print('búið að bæta x í listann', lst)
+## Tekur hlutinn með indexinn 1 úr listanum
+## í þessu tilfelli er það 'y'
+lst.pop(1)
+print('búið að taka index 1 úr listanum', lst, end = ' ')
+print('og index 1 orðið', lst[1])
+## Tekur fyrsta atvik þar sem
+## 'x' kemur fyrir úr listanum
+lst.remove('x')
+print('Fyrsta x-ið var tekið úr lst',lst)
+## Fall sem tekur eyðir öllum
+## x í lst
+def remove_all_x(listinn, value):
+    while value in listinn:
+        listinn.remove(value)
+## Kallar á fallið og setur
+## lst of 1 inn í fallið
+remove_all_x(lst, 1)
+print('Öllum ásum hefur verið eytt út', lst)
+
+## For lykkjur með listum
+for i in lst:
+    print(i)
+for num, item in enumerate(lst, 0):
+    print('Hlutur á index', num, 'er:', item)
